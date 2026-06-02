3e598fc8aaa7eea3070d7ea5d74286ccd78c1f7b
Jeevan Mairale <codinglife1990@gmail.com>
2026-06-03 00:47:22 +0530

    Fix CORS

--- store-crm-mlm/backend/src/server.js
+++ store-crm-mlm/backend/src/server.js
@@ -16,10 +16,11 @@ const app = express();
 app.use(helmet());
 // app.use(cors());
 app.use(cors({
-    origin: [
-        'http://localhost:5173'
-    ],
-    credentials: true
+  origin: [
+    'http://localhost:5173',
+    'https://YOUR-NETLIFY-URL.netlify.app'
+  ],
+  credentials: true
 }));
 app.use(morgan('dev'));
 app.use(express.json());
